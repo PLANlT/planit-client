664a8127afb1b31687df92070ce5c1559a9bb7ad
dodo <eg4rang@naver.com>
2025-06-11 12:07:01 +0900

    [PL-84] Feat: 카카오 로그인 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:planit/repository/auth/auth_repository.dart';
 
 Future<void> main() async {
+  // runApp 실행전 초기화
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // env 로드
   await dotenv.load(fileName: 'assets/.env');
+
+  // Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY'],
+    javaScriptAppKey: dotenv.env['KAKAO_JAVASCRIPT_APP_KEY'],
+  );
 
   runApp(const MyApp());
 }
@@ -21,6 +33,20 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(40.0),
+        child: Column(
+          children: [
+            // 카카오 로그인 테스트
+            ElevatedButton(
+                onPressed: () async {
+                  AuthRepository().kakaoLogin();
+                },
+                child: Text('카카오 로그인'))
+          ],
+        ),
+      ),
+    );
   }
 }
