6cc80f183e2d3129ef0fe30c81917729fc4d0cfc
dodo <eg4rang@naver.com>
2025-06-24 08:45:40 +0900

    [PL-87] Feat: Color 추가

---
+++ lib/theme/planit_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PlanitColors {
+  static const Color primary = Color(0xFFFFCD05);
+
+  static const Color pink = Color(0xFFFF6B9D);
+  static const Color blue = Color(0xFF6BB5FF);
+  static const Color green = Color(0xFF37CD9C);
+  static const Color orange = Color(0xFFFF9C6C);
+  static const Color purple = Color(0xFFAF97FF);
+  static const Color red = Color(0xFFFF6A6C);
+
+  static const Color white01 = Color(0xFFFFFFFF);
+  static const Color white02 = Color(0xFFF3F3F3);
+  static const Color white03 = Color(0xFFD9D9D9);
+  static const Color black04 = Color(0xFFC4C4C4);
+  static const Color black03 = Color(0xFF9A9A9A);
+  static const Color black02 = Color(0xFF5A5A5A);
+  static const Color black01 = Color(0xFF222222);
+
+  static const Color transparent = Colors.transparent;
+}
