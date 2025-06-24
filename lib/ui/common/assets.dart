f1216ce2f37886a36a09b542b849bcafe06497c3
dodo <eg4rang@naver.com>
2025-06-24 19:30:12 +0900

    [PL-87] Icons 추가

---
+++ lib/ui/common/assets.dart
@@ -0,0 +1,11 @@
+// 오탈자 방지를 위해 Assets 경로를 String 변수로 관리합니다
+class Assets {
+  // icons
+  static const String chevronDown = 'assets/icons/chevron_down.svg';
+  static const String chevronLeft = 'assets/icons/chevron_left.svg';
+  static const String chevronRight = 'assets/icons/chevron_right.svg';
+  static const String chevronUp = 'assets/icons/chevron_up.svg';
+  static const String edit = 'assets/icons/edit.svg';
+  static const String more = 'assets/icons/more.svg';
+  static const String x = 'assets/icons/x.svg';
+}
