aa855b1153cb90ba873b68fe0108c956259cafa9
dodo <eg4rang@naver.com>
2025-07-05 02:33:10 +0900

    [PL-127] Chore: StorageKey, DateTime Util 추가

--- lib/service/storage/storage_key.dart
+++ lib/service/storage/storage_key.dart
@@ -6,4 +6,10 @@ class StorageKey {
   // 주의 : bool 타입 저장 시, defaultValue가 false인 것이 자연스러운 네이밍을 사용합니다
   // 예 : isFirstLaunch가 아닌 isNotFirstLaunch가 되어야 초기값이 첫 설치가 됨
   static const isNotFirstLaunch = 'isNotFirstLaunch';
+
+  // 마지막으로 길티프리 모드를 사용한 날짜
+  static const lastGuiltyFreeDate = 'lastGuiltyFreeDate';
+
+  // GuiltyFreeStatus 저장
+  static const guiltyFreeStatus = 'guiltyFreeStatus';
 }
