e925c766536f6b223e71dc12060426434ff92d37
qixiangme <erang903@khu.ac.kr>
2025-07-04 23:56:07 +0900

    fix: task_card 고정값 설정 -> 내부패딩 조정

--- lib/ui/plan/component/task_card.dart
+++ lib/ui/plan/component/task_card.dart
@@ -23,40 +23,40 @@ class taskCard extends StatelessWidget {
         decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(4)),
             color: PlanitColors.white02),
-        width: 320,
+        width: double.infinity,
         height: 56,
-        child: Row(
-          children: [
-            SizedBox(
-              width: 20,
-            ),
-            Expanded(
-              child: PlanitText(title, style: PlanitTypos.body3),
-            ),
-            if (taskType == 'ALL') //나중에 서버님들한테 taskType 뭐 있는 지 물어보기
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            children: [
+              Expanded(
+                child: PlanitText(title, style: PlanitTypos.body3),
+              ),
+              if (taskType == 'ALL') //나중에 서버님들한테 taskType 뭐 있는 지 물어보기
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                  child: SvgPicture.asset(Assets.high),
+                ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                child: SvgPicture.asset(Assets.high),
+                child: SvgPicture.asset(Assets.low),
               ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 2.0),
-              child: SvgPicture.asset(Assets.low),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4),
-              child: GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet(
-                      context: context,
-                      isScrollControlled: false,
-                      builder: (context) {
-                        return TaskMoreBottomSheetView();
-                      },
-                    );
-                  },
-                  child: SvgPicture.asset(Assets.more)),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        isScrollControlled: false,
+                        builder: (context) {
+                          return TaskMoreBottomSheetView();
+                        },
+                      );
+                    },
+                    child: SvgPicture.asset(Assets.more)),
+              ),
+            ],
+          ),
         ),
       ),
     );
