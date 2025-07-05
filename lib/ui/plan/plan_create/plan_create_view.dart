3082a7fa87e4411b97f259d9f1a09f17469712be
qixiangme <erang903@khu.ac.kr>
2025-07-05 17:45:07 +0900

    [PL-142] fix: planitButton 사용 / padding 20- > 16/ SingleChildScroll 추가

--- lib/ui/plan/plan_create/plan_create_view.dart
+++ lib/ui/plan/plan_create/plan_create_view.dart
@@ -4,9 +4,11 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:planit/theme/planit_colors.dart';
 import 'package:planit/theme/planit_typos.dart';
+import 'package:planit/ui/common/comopnent/planit_button.dart';
 import 'package:planit/ui/common/comopnent/planit_chip.dart';
 import 'package:planit/ui/common/comopnent/planit_text.dart';
 import 'package:planit/ui/common/comopnent/planit_text_field.dart';
+import 'package:planit/ui/common/const/planit_button_style.dart';
 import 'package:planit/ui/common/const/planit_chips_style.dart';
 import 'package:planit/ui/common/view/default_layout.dart';
 import 'package:planit/ui/plan/component/custom_chip.dart';
@@ -26,182 +28,174 @@ class PlanCreateView extends HookConsumerWidget {
     final motivationController = useTextEditingController();
 
     return DefaultLayout(
-        child: Column(
-      children: [
-        AppBar(
-            backgroundColor: PlanitColors.transparent,
-            title: PlanitText('내 플랜 만들기',
-                style:
-                    PlanitTypos.body2.copyWith(color: PlanitColors.black01))),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 20.0,
-          children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16).copyWith(top: 28),
-              child: Row(
-                children: [
-                  PlanitText(
-                    '플랜 제목',
-                    style:
-                        PlanitTypos.body2.copyWith(color: PlanitColors.black02),
-                  ),
-                  PlanitText(
-                    '*',
-                    style:
-                        PlanitTypos.body2.copyWith(color: PlanitColors.alert),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: PlanitTextField(
-                hintText: '당신의 목표는 무엇인가요?',
-                controller: titleController,
-                onChanged: viewmodel.updateTitle,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  PlanitText('다짐 문장',
-                      style: PlanitTypos.body2
-                          .copyWith(color: PlanitColors.black02)),
-                  PlanitText(' (20자 내)',
-                      style: PlanitTypos.body3
-                          .copyWith(color: PlanitColors.black03)),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: PlanitTextField(
-                hintText: '이 플랜을 지속할 수 있는 요소가 있나요?',
-                controller: motivationController,
-                onChanged: viewmodel.updateMotivation,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: [
-                  PlanitText('행성 아이콘 선택',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 16,
-                          color: PlanitColors.black02)),
-                  PlanitText(
-                    '*',
-                    style:
-                        PlanitTypos.body2.copyWith(color: PlanitColors.alert),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16).copyWith(top: 12),
-              child: PlanetWrapGrid(
-                  selectedIcon: state.icon,
-                  onSelect: (asset) {
-                    viewmodel.updateIcon(asset);
-                  }),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16).copyWith(top: 12),
-              child: PlanitText('목표일 설정',
+        child: SingleChildScrollView(
+      child: Column(
+        children: [
+          AppBar(
+              backgroundColor: PlanitColors.transparent,
+              title: PlanitText('내 플랜 만들기',
                   style:
-                      PlanitTypos.body2.copyWith(color: PlanitColors.black02)),
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: GestureDetector(
-                      onTap: () {
-                        //DatePickerUI 구현
-                      },
-                      child: PlanCreateChip(
-                        bordercolor: PlanitColors.white03,
-                        title: '25.05.01',
-                        textcolor: PlanitColors.black03,
-                        backgroundcolor: PlanitColors.white01,
-                      )),
-                ),
-                PlanitText('까지 | D-',
-                    style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                        color: PlanitColors.black03)),
-              ],
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16).copyWith(top: 12),
-              child: Row(
-                children: [
-                  PlanitText('플랜 진행 여부',
+                      PlanitTypos.body2.copyWith(color: PlanitColors.black01))),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 20.0,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20)
+                    .copyWith(top: 28),
+                child: Row(
+                  children: [
+                    PlanitText(
+                      '플랜 제목',
                       style: PlanitTypos.body2
-                          .copyWith(color: PlanitColors.black02)),
-                  PlanitText(
-                    '*',
-                    style:
-                        PlanitTypos.body2.copyWith(color: PlanitColors.alert),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                spacing: 8,
-                children: [
-                  GestureDetector(
-                      onTap: () => viewmodel.updatePlanStatus('IN_PROGRESS'),
-                      child: PlanitChip(
-                          chipColor: state.planStatus == 'IN_PROGRESS'
-                              ? PlanitChipColor.black
-                              : PlanitChipColor.gray,
-                          label: '진행 중')),
-                  GestureDetector(
-                      onTap: () => viewmodel.updatePlanStatus('PAUSED'),
-                      child: PlanitChip(
-                          chipColor: state.planStatus == 'PAUSED'
-                              ? PlanitChipColor.black
-                              : PlanitChipColor.gray,
-                          label: '중단')),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16)
-                    .copyWith(top: 20),
-                child: Container(
-                  height: 48,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadiusGeometry.circular(36.0),
-                      color: PlanitColors.black01),
-                  child: Center(
-                    child: PlanitText(
-                      '플랜 만들기',
-                      style: PlanitTypos.body3.copyWith(
-                        color: PlanitColors.white01,
-                      ),
+                          .copyWith(color: PlanitColors.black02),
                     ),
-                  ),
+                    PlanitText(
+                      '*',
+                      style:
+                          PlanitTypos.body2.copyWith(color: PlanitColors.alert),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
-        ),
-      ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: PlanitTextField(
+                  hintText: '당신의 목표는 무엇인가요?',
+                  controller: titleController,
+                  onChanged: viewmodel.updateTitle,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    PlanitText('다짐 문장',
+                        style: PlanitTypos.body2
+                            .copyWith(color: PlanitColors.black02)),
+                    PlanitText(' (20자 내)',
+                        style: PlanitTypos.body3
+                            .copyWith(color: PlanitColors.black03)),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: PlanitTextField(
+                  hintText: '이 플랜을 지속할 수 있는 요소가 있나요?',
+                  controller: motivationController,
+                  onChanged: viewmodel.updateMotivation,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    PlanitText('행성 아이콘 선택',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                            color: PlanitColors.black02)),
+                    PlanitText(
+                      '*',
+                      style:
+                          PlanitTypos.body2.copyWith(color: PlanitColors.alert),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20)
+                    .copyWith(top: 12),
+                child: PlanetWrapGrid(
+                    selectedIcon: state.icon,
+                    onSelect: (asset) {
+                      viewmodel.updateIcon(asset);
+                    }),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20)
+                    .copyWith(top: 12),
+                child: PlanitText('목표일 설정',
+                    style: PlanitTypos.body2
+                        .copyWith(color: PlanitColors.black02)),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: GestureDetector(
+                        onTap: () {
+                          //DatePickerUI 구현
+                        },
+                        child: PlanCreateChip(
+                          bordercolor: PlanitColors.white03,
+                          title: '25.05.01',
+                          textcolor: PlanitColors.black03,
+                          backgroundcolor: PlanitColors.white01,
+                        )),
+                  ),
+                  PlanitText('까지 | D-',
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: PlanitColors.black03)),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20)
+                    .copyWith(top: 12),
+                child: Row(
+                  children: [
+                    PlanitText('플랜 진행 여부',
+                        style: PlanitTypos.body2
+                            .copyWith(color: PlanitColors.black02)),
+                    PlanitText(
+                      '*',
+                      style:
+                          PlanitTypos.body2.copyWith(color: PlanitColors.alert),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  spacing: 8,
+                  children: [
+                    GestureDetector(
+                        onTap: () => viewmodel.updatePlanStatus('IN_PROGRESS'),
+                        child: PlanitChip(
+                            chipColor: state.planStatus == 'IN_PROGRESS'
+                                ? PlanitChipColor.black
+                                : PlanitChipColor.gray,
+                            label: '진행 중')),
+                    GestureDetector(
+                        onTap: () => viewmodel.updatePlanStatus('PAUSED'),
+                        child: PlanitChip(
+                            chipColor: state.planStatus == 'PAUSED'
+                                ? PlanitChipColor.black
+                                : PlanitChipColor.gray,
+                            label: '중단')),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: PlanitButton(
+                      onPressed: () {},
+                      buttonColor: PlanitButtonColor.black,
+                      buttonSize: PlanitButtonSize.large,
+                      label: '플랜 만들기'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     ));
   }
 }
