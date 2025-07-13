dc06649e3bd5ad31c25ca1d0c944b52f02f736e6
dodo <eg4rang@naver.com>
2025-07-13 13:21:46 +0900

    [PL-156] Chore: 테스트/빌드용 파일

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:planit/ui/common/comopnent/planit_text_field.dart';
 import 'package:planit/ui/common/comopnent/planit_toast.dart';
 import 'package:planit/ui/common/const/planit_button_style.dart';
 import 'package:planit/ui/common/const/planit_chips_style.dart';
+import 'package:planit/ui/onboarding/onboarding_view.dart';
 import 'package:planit/ui/splash_view.dart';
 
 Future<void> main() async {
@@ -68,6 +69,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
                 child: Text('루트탭으로 꼬우'),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => OnboardingView(),
+                  ),
+                ),
+                child: Text('온보딩'),
               ),
               Text(
                 '로그인 연동 테스트',
