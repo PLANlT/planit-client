600dd316dd5bf511bb56a4dd19c8d21f0ca64c7f
qixiangme <erang903@khu.ac.kr>
2025-07-13 21:34:51 +0900

    [PL-122] Feat: 플랜UI > 진행중인 플랜 불러오기 API 연동

---
+++ lib/data_source/plan/plan_data_source.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:planit/core/api_response.dart';
+import 'package:planit/data_source/plan/reponse_body/plan_response_body.dart';
+import 'package:planit/service/network/dio_service.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'plan_data_source.g.dart';
+
+final Provider<PlanDataSource> planDataSourceProvider =
+    Provider<PlanDataSource>(
+  (ref) => PlanDataSource(ref.read(dioServiceProvider)),
+);
+
+@RestApi()
+abstract class PlanDataSource {
+  factory PlanDataSource(Dio dio) = _PlanDataSource;
+
+  @GET('/planit/plans')
+  Future<ApiResponse<PlanListResponseBody>> getPlanLists();
+
+  
+
+}
--- lib/repository/plan/plan_repository.dart
+++ lib/repository/plan/plan_repository.dart
@@ -1,6 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/rendering.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:planit/core/api_response.dart';
+import 'package:planit/core/error_message.dart';
 import 'package:planit/core/repository_result.dart';
+import 'package:planit/data_source/plan/plan_data_source.dart';
+import 'package:planit/data_source/plan/reponse_body/plan_response_body.dart';
 import 'package:planit/repository/plan/model/plan_detail_model.dart';
 import 'package:planit/repository/plan/model/plan_model.dart';
 import 'package:planit/repository/plan/model/task_model.dart';
@@ -8,134 +13,38 @@ import 'package:planit/ui/common/assets.dart';
 
 final AutoDisposeProvider<PlanRepository> planRepositoryProvider =
     Provider.autoDispose<PlanRepository>(
-  (ref) => PlanRepository(),
+  (ref) => PlanRepository(planDataSource: ref.read(planDataSourceProvider)),
 );
 
 class PlanRepository {
-  const PlanRepository();
+  final PlanDataSource _planDataSource;
 
-Future<RepositoryResult<List<PlanModel>>> getActivePlanList() async {
-  return SuccessRepositoryResult(data: [
-    PlanModel(
-      planId: 0,
-      title: 'TOEIC 고득점',
-      motivation: '매일 조금씩 나아지는..',
-      totalTask: 5,
-      dDay: 12,
-      icon: Assets.planet1,
-    ),
-    PlanModel(
-      planId: 1,
-      title: '독서 습관',
-      motivation: '독서 습관을 기르자',
-      totalTask: 2,
-      dDay: 7,
-      icon: Assets.planet3,
-    ),
-    PlanModel(
-      planId: 2,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: Assets.planet6,
-    ),
-    PlanModel(
-      planId: 2,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-    PlanModel(
-      planId: 2,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-    PlanModel(
-      planId: 2,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-  ]);
-}
+  const PlanRepository({
+    required PlanDataSource planDataSource,
+  }) : _planDataSource = planDataSource;
 
-Future<RepositoryResult<List<PlanModel>>> getPausePlanList() async {
-  return SuccessRepositoryResult(data: [
-    PlanModel(
-      planId: 5,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: Assets.planet1,
-    ),
-    PlanModel(
-      planId: 5,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-    PlanModel(
-      planId: 5,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-    PlanModel(
-      planId: 5,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-    PlanModel(
-      planId: 5,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-    PlanModel(
-      planId: 5,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-    PlanModel(
-      planId: 5,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-    PlanModel(
-      planId: 5,
-      title: '헬스장 꾸준히 가기',
-      motivation: '작심삼일 10번이면 한달이다',
-      totalTask: 3,
-      dDay: 20,
-      icon: 'assets/planets/planet6.svg',
-    ),
-  ]);
-}
+  Future<RepositoryResult<List<PlanModel>>> getActivePlanList() async {
+    try {
+      final ApiResponse<PlanListResponseBody> result =
+          await _planDataSource.getPlanLists();
 
+      final data = result.data;
+
+      if (data.planStatus == 'IN_PROGRESS') {
+        final plans = data.plans;
+        final models = plans.map((e) => PlanModel.fromResponse(e)).toList();
+        return SuccessRepositoryResult(data: models);
+      } else {
+        return SuccessRepositoryResult(data: []);
+      }
+    } on DioException catch (e) {
+      return FailureRepositoryResult(error: e, messages: [networkErrorMsg]);
+    }
+  }
+//
+  Future<RepositoryResult<List<PlanModel>>> getPausePlanList() async {
+    return SuccessRepositoryResult(data: []);
+  }
 
   Future<RepositoryResult<PlanDetailModel>> getPlanDetailByPlanId(
       int planId) async {
