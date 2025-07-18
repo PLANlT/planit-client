c657f4c00414d343077fd3e8d96ad93d6a15ceec
dodo <eg4rang@naver.com>
2025-07-18 22:18:39 +0900

    [PL-161] Feat: 로그인 UI 구현 - Repository 통해 Auth API 사용 - 회원가입 완료 여부에 따라 로그인 완료 처리 또는 약관동의 유도

--- lib/ui/login/login_view.dart
+++ lib/ui/login/login_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:planit/core/loading_status.dart';
 import 'package:planit/theme/planit_colors.dart';
@@ -7,10 +10,13 @@ import 'package:planit/theme/planit_typos.dart';
 import 'package:planit/ui/common/comopnent/planit_loading.dart';
 import 'package:planit/ui/common/comopnent/planit_text.dart';
 import 'package:planit/ui/common/view/default_layout.dart';
+import 'package:planit/ui/common/view/root_tab.dart';
 import 'package:planit/ui/login/login_state.dart';
 import 'package:planit/ui/login/login_view_model.dart';
+import 'package:planit/ui/login/tos_view.dart';
 
 import '../common/assets.dart';
+import '../common/comopnent/planit_toast.dart';
 import 'component/login_buttons.dart';
 
 class LoginView extends HookConsumerWidget {
@@ -22,6 +28,31 @@ class LoginView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final LoginViewModel viewModel = ref.read(loginViewModelProvider.notifier);
     final LoginState state = ref.watch(loginViewModelProvider);
+
+    final FToast toast = FToast().init(context);
+
+    useEffect(() {
+      // 로그인 완료
+      if (state.isLoginCompleted != null) {
+        // 로그인이 완료되었다면 루트탭으로 이동
+        // 완료되지 않았다면 약관 동의 화면으로 이동 (회원가입)
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          context.goNamed(
+              state.isLoginCompleted! ? RootTab.routeName : TosView.routeName);
+          viewModel.routingRefresh();
+        });
+      }
+      return null;
+    }, [state.isLoginCompleted]);
+
+    useEffect(() {
+      if (state.errorMessage.isNotEmpty) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          toast.showToast(child: PlanitToast(label: state.errorMessage));
+        });
+      }
+      return null;
+    }, [state.errorMessage]);
 
     return DefaultLayout(
       child: Stack(
@@ -53,7 +84,7 @@ class LoginView extends HookConsumerWidget {
                   spacing: 12.0,
                   children: [
                     GoogleButton(
-                      onPressed: () => viewModel.googleLogin,
+                      onPressed: () => viewModel.googleLogin(),
                     ),
                     KakaoButton(
                       onPressed: () => viewModel.kakaoLogin(),
