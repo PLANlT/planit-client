6656093639279fca7cdf1d5a4897a5c611a0ecae
dodo <eg4rang@naver.com>
2025-07-26 23:15:33 +0900

    [PL-130] Feat: 로그인 시 길티프리 종료일 불러오기 - 길티프리 날짜를 기본적으로 로컬 데이터로 관리 - 기기 변경 또는 앱 삭제 시 로컬 데이터가 날라가므로 서버에서 값을 불러옴

--- lib/utils/date_time.dart
+++ lib/utils/date_time.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 
 // String을 DateTime으로 변환
@@ -6,7 +5,6 @@ DateTime? stringToDateTime(String dateString) {
   try {
     return DateFormat('yyyy-MM-dd HH:mm:ss.SSS').parse(dateString);
   } catch (e) {
-    debugPrint('String to DateTime 실패: $e');
-    return null;
+    return DateTime.tryParse(dateString);
   }
 }
