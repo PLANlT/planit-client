d5e8ac5d6aa7e0f49f05f0b938c5148a2d74f8ad
dodo <eg4rang@naver.com>
2025-07-29 22:30:18 +0900

    [PL-162] Fix: 아카이브 디테일뷰 UI 수정

--- lib/ui/archiving/archiving_detail/archiving_detail_view.dart
+++ lib/ui/archiving/archiving_detail/archiving_detail_view.dart
@@ -7,8 +7,8 @@ import 'package:planit/core/loading_status.dart';
 import 'package:planit/theme/planit_colors.dart';
 import 'package:planit/theme/planit_typos.dart';
 import 'package:planit/ui/archiving/archiving_restart/archiving_restart_view.dart';
-import 'package:planit/ui/common/assets.dart';
 import 'package:planit/ui/common/comopnent/planit_button.dart';
+import 'package:planit/ui/common/comopnent/planit_loading.dart';
 import 'package:planit/ui/common/comopnent/planit_text.dart';
 import 'package:planit/ui/common/const/planit_button_style.dart';
 import 'package:planit/ui/common/view/default_layout.dart';
@@ -18,15 +18,21 @@ import 'package:planit/ui/plan/plan_detail/plan_detail_view_model.dart';
 
 class ArchivingDetailView extends HookConsumerWidget {
   final int planId;
+
   static String get routeName => 'archiving-detail';
+
   const ArchivingDetailView({super.key, required this.planId});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final PlanDetailState state =
-        ref.watch(planDetailViewModelProvider(planId));
-    final PlanDetailViewModel viewModel =
-        ref.read(planDetailViewModelProvider(planId).notifier);
+    final PlanDetailState state = ref.watch(
+      planDetailViewModelProvider(planId),
+    );
+    final PlanDetailViewModel viewModel = ref.read(
+      planDetailViewModelProvider(planId).notifier,
+    );
+
+    final Size deviceSize = MediaQuery.of(context).size;
 
     useEffect(() {
       WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -35,126 +41,137 @@ class ArchivingDetailView extends HookConsumerWidget {
       return null;
     }, []);
 
-    if (state.loadingStatus == LoadingStatus.loading) {
-      return Center(child: CircularProgressIndicator());
-    }
-    if (state.planDetail == null) {
-      return Center(child: CircularProgressIndicator());
-    }
-
     return DefaultLayout(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        AppBar(
-          backgroundColor: PlanitColors.transparent,
-        ),
-        SizedBox(
-          width: double.infinity,
-          height: 136,
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: SvgPicture.asset(
-                  '${state.planDetail!.icon}.svg',
-                  width: 120,
-                  height: 120,
+      title: '',
+      child: state.planDetail == null
+          ? Center(child: PlanitLoading())
+          : Stack(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      height: 136,
+                      child: Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment.center,
+                            child: SvgPicture.asset(
+                              '${state.planDetail!.icon}.svg',
+                              width: 120,
+                              height: 120,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                      ).copyWith(top: 12.0),
+                      child: PlanitText(
+                        state.planDetail!.title,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    if (state.planDetail!.motivation.isNotEmpty)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                        ).copyWith(top: 4.0),
+                        child: PlanitText(
+                          state.planDetail!.motivation,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: PlanitColors.black03,
+                          ),
+                        ),
+                      ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                      ).copyWith(top: 40),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Column(
+                          spacing: 8,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            PlanitText(
+                              '내가 해낸 태스크들',
+                              style: PlanitTypos.title3.copyWith(
+                                color: PlanitColors.black01,
+                              ),
+                            ),
+                            PlanitText(
+                              '한 걸음씩 내딛던 당신, 참 멋졌어요.',
+                              style: PlanitTypos.body3.copyWith(
+                                color: PlanitColors.black03,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    // 최대 높이 지정
+                    Container(
+                      constraints: BoxConstraints(
+                        maxHeight: deviceSize.height / 12 * 5,
+                      ),
+                      child: ListView.separated(
+                        shrinkWrap: true,
+                        padding: EdgeInsets.zero,
+                        itemCount: state.planDetail!.tasks.length,
+                        itemBuilder: (context, index) {
+                          final item = state.planDetail!.tasks[index];
+                          return TaskCard(
+                            title: item.title,
+                            taskType: item.taskType,
+                            taskId: item.taskId,
+                            showMore: false,
+                          );
+                        },
+                        separatorBuilder: (context, index) =>
+                            SizedBox(height: 12),
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                      ).copyWith(bottom: 40),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: PlanitButton(
+                          onPressed: () {
+                            context.pushNamed(
+                              ArchivingRestartView.routeName,
+                              pathParameters: {
+                                'planId': planId.toString(),
+                                'title': state.planDetail!.title
+                              },
+                            );
+                          },
+                          buttonColor: PlanitButtonColor.black,
+                          buttonSize: PlanitButtonSize.large,
+                          label: '다시 시작하기',
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              Positioned(
-                top: 8,
-                right: 20,
-                child: GestureDetector(
-                  onTap: () {
-                    // showModalBottomSheet(
-                    //   context: context,
-                    //   builder: (context) {
-                    //     return PlanMoreBottomSheet();
-                    //   },
-                    // );
-                  },
-                  child: SvgPicture.asset(
-                    Assets.more,
-                    width: 24,
-                    height: 24,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 12.0),
-          child: PlanitText(state.planDetail!.title,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-              )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 4.0, bottom: 40),
-          child: PlanitText(state.planDetail!.motivation,
-              style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  color: PlanitColors.black03)),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Align(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                spacing: 8,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  PlanitText('내가 해낸 태스크들', style: PlanitTypos.title3),
-                  PlanitText('한 걸음씩 내딛던 당신, 참 멋졌어요.',
-                      style: PlanitTypos.body3
-                          .copyWith(color: PlanitColors.black03)),
-                ],
-              )),
-        ),
-        SizedBox(
-          child: ListView.builder(
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: state.planDetail!.tasks.length,
-            itemBuilder: (context, index) {
-              final item = state.planDetail!.tasks[index];
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 12),
-                child: TaskCard(
-                  title: item.title,
-                  taskType: item.taskType,
-                  taskId: item.taskId,
-                ),
-              );
-            },
-          ),
-        ),
-        Spacer(),
-        Padding(
-          padding:
-              const EdgeInsets.symmetric(horizontal: 20).copyWith(bottom: 76),
-          child: SizedBox(
-            width: double.infinity,
-            child: PlanitButton(
-                onPressed: () {
-                  context.pushNamed(
-                    ArchivingRestartView.routeName,
-                    pathParameters: {
-                      'planId': planId.toString(),
-                      'title': state.planDetail!.title
-                    },
-                  );
-                },
-                buttonColor: PlanitButtonColor.black,
-                buttonSize: PlanitButtonSize.large,
-                label: '다시 시작하기'),
-          ),
-        )
-      ],
-    ));
+                if (state.loadingStatus == LoadingStatus.loading)
+                  Center(
+                    child: PlanitLoading(),
+                  )
+              ],
+            ),
+    );
   }
 }
--- lib/ui/plan/component/task_card.dart
+++ lib/ui/plan/component/task_card.dart
@@ -4,19 +4,20 @@ import 'package:planit/theme/planit_colors.dart';
 import 'package:planit/theme/planit_typos.dart';
 import 'package:planit/ui/common/assets.dart';
 import 'package:planit/ui/common/comopnent/planit_text.dart';
-import 'package:planit/ui/plan/plan_detail/bottom_sheet/plan_more/plan_more_bottom_sheet_view.dart';
 import 'package:planit/ui/plan/plan_detail/bottom_sheet/task_more/task_more_bottom_sheet_view.dart';
 
 class TaskCard extends StatelessWidget {
   final String title;
   final String taskType;
   final int taskId;
+  final bool showMore;
 
   const TaskCard({
     super.key,
     required this.title,
     required this.taskId,
     required this.taskType,
+    this.showMore = true,
   });
 
   @override
@@ -57,23 +58,24 @@ class TaskCard extends StatelessWidget {
                   child: SvgPicture.asset(Assets.high),
                 ),
               ],
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 4),
-                child: GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet(
-                      context: context,
-                      isScrollControlled: false,
-                      builder: (context) {
-                        return TaskMoreBottomSheetView(
-                          taskId: taskId,
-                        );
-                      },
-                    );
-                  },
-                  child: SvgPicture.asset(Assets.more),
+              if (showMore)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        isScrollControlled: false,
+                        builder: (context) {
+                          return TaskMoreBottomSheetView(
+                            taskId: taskId,
+                          );
+                        },
+                      );
+                    },
+                    child: SvgPicture.asset(Assets.more),
+                  ),
                 ),
-              ),
             ],
           ),
         ),
